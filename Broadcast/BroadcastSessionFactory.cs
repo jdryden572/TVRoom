1a5accb371824b3dabd092b7d08f3212570892e6
James Dryden <jdryden572@gmail.com>
2024-07-07 08:15:54 -0400

    Switch to HLS event to see if it works better with chromecast

--- Broadcast/BroadcastSessionFactory.cs
+++ Broadcast/BroadcastSessionFactory.cs
@@ -39,8 +39,8 @@ namespace LivingRoom.Broadcast
 
         private string BuildFFmpegArguments(string input, TranscodeOptions options, DirectoryInfo transcodeDirectory)
         {
-            string deleteSegments = _transcodeConfig.HlsDeleteSegments ? "-hls_flags delete_segments" : string.Empty;
-            var hlsSettings = $"-f hls -hls_time {_transcodeConfig.HlsTime} -hls_list_size {_transcodeConfig.HlsListSize} {deleteSegments}";
+            //string deleteSegments = _transcodeConfig.HlsDeleteSegments ? "-hls_flags delete_segments" : string.Empty;
+            var hlsSettings = $"-f hls -hls_time {_transcodeConfig.HlsTime} -hls_segment_type mpegts -start_number 0 -hls_playlist_type event -hls_list_size 0";
 
             var playlist = Path.Join(transcodeDirectory.FullName, _transcodeConfig.HlsPlaylistName);
             return $"-y {options.InputVideoOptions} -i {input} -c:a aac -ac 2 {options.OutputVideoOptions} {hlsSettings} {playlist}";
